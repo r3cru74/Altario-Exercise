0ade30278620b6c0546756a647d78a84b1563e2c
r3cru74 <r3cru74@users.noreply.github.com>
2022-07-02 09:15:15 +0100

    add components
    add page generator and payment

--- Altario/src/app/app.module.ts
+++ Altario/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneratorPageComponent } from './generator-page/generator-page.component';
+import { PaymentPageComponent } from './payment-page/payment-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneratorPageComponent,
+    PaymentPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ Altario/src/app/generator-page/generator-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generator-page',
+  templateUrl: './generator-page.component.html',
+  styleUrls: ['./generator-page.component.css']
+})
+export class GeneratorPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
