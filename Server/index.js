2a85e848e0ec36e164afa5e40b69c57461539466
r3cru74 <r3cru74@users.noreply.github.com>
2022-07-03 11:04:02 +0100

    server
    backend with CRUD

---
+++ Server/index.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql2')
+
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(cors());
+app.use(bodyparser.json());
+
+// Connection to database
+
+const database = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'db_altario',
+    port:3306
+});
+
+//check database
+
+database.connect(err=>{
+    if(err){console.log(err,'db error');}
+
+    console.log('Database Connected');
+})
+
+
+//Get data from Database
+
+app.get('/Payment',(req,res)=>{
+    console.log('get payment')
+
+    let query = `SELECT * FROM Payment`;
+
+    database.query(query,(err,result)=>{
+        if(err){console.log(err,'db query error');}
+
+        if(result.length>0){
+            res.send({
+                message:'all payments',
+                data:result
+            })
+        }
+    });
+
+});
+
+//get single Data from Database
+app.get('/Payment/:id',(req,res)=>{
+    let gID = req.params.id;
+
+    let query = `SELECT * FROM Payment WHERE id=${gID}`;
+
+    database.query(query,(err,result)=>{
+        if(err){console.log(err,'db query error');}
+
+        if(result.length>0){
+            res.send({
+                message:'get id payments',
+                data:result
+            })
+        }
+    });
+});
+
+//Create data into database
+
+app.post('/Payment',(req,res)=>{
+    console.log(req.body,'Post data');
+
+    let name = req.body.name;
+    let amount = req.body.amount;
+    let code = req.body.code;
+    let grid = req.body.grid;
+
+    let query = `INSERT INTO Payment(name,amount,code,grid) values('${name}','${amount}','${code}','${grid}')`;
+
+    database.query(query,(err,result)=>{
+        if(err){console.log(err,'db query error');}
+
+            res.send({
+                message:'add data to payments',
+                data:result
+            })
+        
+    });
+})
+
+//Update database
+
+app.put('/Payment',(req,res)=>{
+    console.log(req.body,'Update data');
+
+    let id = req.params.id;
+    let name = req.body.name;
+    let amount = req.body.amount;
+    let code = req.body.code;
+    let grid = req.body.grid;
+
+    let query = `UPDATE Payment SET name ='${name}', amount='${amount}', code='${code}', grid='${grid}' WHERE id=${id}`;
+
+    database.query(query,(err,result)=>{
+        if(err){console.log(err,'db query error');}
+
+            res.send({
+                message:'Update data to payments',
+                data:result
+            })
+        
+    });
+})
+
+//Delete item From Database
+
+app.delete('/Payment',(req,res)=>{
+    console.log(req.body,'Delete data');
+
+    let id = req.params.id;
+
+    let query = `DELETE FROM Payment WHERE id='${id}'`;
+
+    database.query(query,(err,result)=>{
+        if(err){console.log(err,'db query error');}
+
+            res.send({
+                message:'Delete data to payments',
+                data:result
+            })
+        
+    });
+})
+
+
+
+app.listen(port,()=>{
+    console.log('Server running on port:'+port);
+});
